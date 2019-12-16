fbe9631352e6ee03e7ca168e2499e257557eaee6
Aditi Bhat <aditiabhat@gmail.com>
2019-12-16 09:52:08 +0530

    Updates

---
+++ precache-manifest.873cd1ad9aace5e9a5271b314ee4c85d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3709b23be4c7781a21cc01b6f8622fe1",
+    "url": "/my-website/index.html"
+  },
+  {
+    "revision": "b6a0f18bb39046a36eae",
+    "url": "/my-website/static/css/2.a641e29a.chunk.css"
+  },
+  {
+    "revision": "9bf71936c6e4d442f554",
+    "url": "/my-website/static/css/main.fa9996ac.chunk.css"
+  },
+  {
+    "revision": "b6a0f18bb39046a36eae",
+    "url": "/my-website/static/js/2.24ccae34.chunk.js"
+  },
+  {
+    "revision": "9bf71936c6e4d442f554",
+    "url": "/my-website/static/js/main.68bdd9e8.chunk.js"
+  },
+  {
+    "revision": "af64d6f2c937ca42352c",
+    "url": "/my-website/static/js/runtime-main.b9f5713c.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/my-website/static/media/logo.25bf045c.svg"
+  }
+]);
