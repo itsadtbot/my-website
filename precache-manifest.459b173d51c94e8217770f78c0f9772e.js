b2e9f3dd74510eb2e4f4521c08b8d3ad6231be0c
Aditi Bhat <aditiabhat@gmail.com>
2020-01-02 23:18:45 +0530

    Updates

---
+++ precache-manifest.459b173d51c94e8217770f78c0f9772e.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "178d0e2f5d44b6c50dece630139f26d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "103b4ff5554a71f30585",
+    "url": "/static/css/2.2aa135dd.chunk.css"
+  },
+  {
+    "revision": "49742f472130705407a1",
+    "url": "/static/css/main.fa9996ac.chunk.css"
+  },
+  {
+    "revision": "103b4ff5554a71f30585",
+    "url": "/static/js/2.16372b6c.chunk.js"
+  },
+  {
+    "revision": "49742f472130705407a1",
+    "url": "/static/js/main.7377b857.chunk.js"
+  },
+  {
+    "revision": "1bcf67dcf5603f73b617",
+    "url": "/static/js/runtime-main.4bbb00a6.js"
+  },
+  {
+    "revision": "e99162768f42965a5e20e445daebe6e6",
+    "url": "/static/media/ColourPsych.e9916276.pdf"
+  },
+  {
+    "revision": "17f37ad09221142c365b9a7426b16eec",
+    "url": "/static/media/Coursera-HeuristicEval.17f37ad0.pdf"
+  },
+  {
+    "revision": "6d301cb0cade4a97493feb2a0fcb10b7",
+    "url": "/static/media/Hipmunk-UserTest.6d301cb0.pdf"
+  },
+  {
+    "revision": "54d474c53d80db35daec76a4bb42e79e",
+    "url": "/static/media/IEEEWIECONPaperID154.54d474c5.pdf"
+  },
+  {
+    "revision": "483a90769eb70a3ba2abd21adcd166ea",
+    "url": "/static/media/Instagram_User_Assessment.483a9076.pdf"
+  },
+  {
+    "revision": "382405a9f2914b16eb6f0a45f957b55a",
+    "url": "/static/media/SUS.382405a9.pdf"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/static/media/revicons.2feb69cc.eot"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.dc7aaeb94817070922232723dbe14b20.js"
+  "/precache-manifest.459b173d51c94e8217770f78c0f9772e.js"
 );
 
 self.addEventListener('message', (event) => {
