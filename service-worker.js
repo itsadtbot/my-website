4495b2cf5c3dec56b4e0017cbf91dfd0591678ad
Aditi Bhat <aditiabhat@gmail.com>
2020-01-06 19:58:13 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9f051e782dcaf9c953924bd2be3f8a3a.js"
+  "/precache-manifest.d55f691626595a97469c0ab1ae9f4b89.js"
 );
 
 self.addEventListener('message', (event) => {
